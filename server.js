152ec3e8b3b06f244daa88e8922ad55e50c3446f
B.O.A Addington <addington.bjorn@gmail.com>
2022-11-11 17:31:44 -0800

    final repo commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+app.use(require('./routes/API/index'));
 
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
